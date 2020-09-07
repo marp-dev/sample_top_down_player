5613b826b1b28770ddaccbe40563bf789fa9c0c8
Miguel Angel Rodriguez Perez <miguelachoo@gmail.com>
2020-09-06 22:03:51 -0300

    formation has positions exported array.

--- testgame/scripts/weapons/WeaponIcon.gd
+++ testgame/scripts/weapons/WeaponIcon.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+var picker
+
 func _ready():
 	connect("body_entered", self, "on_body_entered")
 
@@ -7,3 +9,8 @@ func on_body_entered(body):
 	if body.is_in_group('player'):
 		body.add_weapon(global.weapons.weapon.instance())
 		queue_free()
+	if body.is_in_group('formation'):
+		picker = body.get_weapon(self)
+
+func pick():
+	return global.weapons.weapon.instance()
