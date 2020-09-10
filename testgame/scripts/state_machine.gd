ad72d711cbd420f23baa019a8dfabbc24b425e25
Miguel Angel Rodriguez Perez <miguelachoo@gmail.com>
2020-09-10 16:05:53 -0300

    state machine stack. passing props to change state

--- testgame/scripts/state_machine.gd
+++ testgame/scripts/state_machine.gd
@@ -2,9 +2,11 @@ extends KinematicBody2D
 
 var current_state
 var states
+var stack
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	stack = ['idle']
 	states = {
 		"idle": global.states.idle.new()
 	}
@@ -16,6 +18,15 @@ func _input(event):
 func _process(delta):
 	current_state.update(delta)
 
-func state_change(state = "idle"):
+func state_change(state = false, props = {}):
+	#if(current_state != null and stack.size() > 1):
+	#	current_state.exit()
+	#	stack.remove(0)
+	
+	if(not state or states[state] == null):
+		state = stack[0]
+	else:
+		stack.push_front(state)
 	current_state = states[state]
-	current_state.enter({ "owner": self })
+	props['owner'] = self
+	current_state.enter(props)
--- testgame/scripts/weapons/WeaponIcon.gd
+++ testgame/scripts/weapons/WeaponIcon.gd
@@ -1,13 +1,15 @@
 extends Area2D
 
 var picker
+var weapon
 
 func _ready():
+	weapon = global.weapons.weapon
 	connect("body_entered", self, "on_body_entered")
 
 func on_body_entered(body):
-	if body.is_in_group('player'):
-		body.add_weapon(global.weapons.weapon.instance())
+	if body.is_in_group('player') or picker == body:
+		body.add_weapon(weapon.instance())
 		queue_free()
 	if body.is_in_group('formation'):
 		picker = body.get_weapon(self)
