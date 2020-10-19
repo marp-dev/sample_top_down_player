b1e5e4edf79805876c29199063014a4de607bc74
Miguel Angel Rodriguez Perez <miguelachoo@gmail.com>
2020-10-19 16:03:42 -0300

    formation pick weapon

--- testgame/scripts/player.gd
+++ testgame/scripts/player.gd
@@ -7,20 +7,19 @@ var current_weapon
 func _ready():
 	._ready()
 	label = $Label
-	states.movement = global.states.movement.new()
 	add_to_group("player")
 
 func _input(event):
 	if Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_down"):
-		state_change("movement")
+		state_change(STATE_MOVEMENT)
 	else:
-		state_change("idle")
+		state_change(STATE_IDLE)
 	if current_weapon and Input.is_action_pressed("ui_fire"):
 		current_weapon.fire()
 	._input(event)
 
 func _process(delta):
-	logging += "state: " + current_state.name + "\n"
+	logging += "state: " + stack[0].name + "\n"
 	look_at(get_global_mouse_position())
 	._process(delta)
 	label.text = logging
